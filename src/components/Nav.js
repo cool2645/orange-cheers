667f0f38f6a1a9e994db8fa96b3ec5483bcb0f51
LI JIAHAO <lijiahao99131@gmail.com>
2018-06-09 01:16:01 +0800

    :recycle: add config file
    Signed-off-by: LI JIAHAO <lijiahao99131@gmail.com>

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, NavLink, Link } from 'react-router-dom'
+import { BrowserRouter as Router, NavLink } from 'react-router-dom'
 import Typed from 'typed.js'
 import Sidebar from 'react-sidebar';
 import '../styles/Nav.css'
+import { site, nav } from '../config'
 
 class Nav extends Component {
   constructor() {
@@ -11,9 +12,23 @@ class Nav extends Component {
       collapse: "",
       sidebarOpen: false
     };
-    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    this.setSidebarOpen = this.setSidebarOpen.bind(this);
+    this.retype = this.retype.bind(this);
     document.onmousewheel = this.handleCollapse.bind(this);
     document.addEventListener("DOMMouseScroll", this.handleCollapse.bind(this))
+  }
+
+  setSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+
+  retype(timeout) {
+    // BUG: https://github.com/mattboldt/typed.js/issues/283
+    console.log(this.typed.strings);
+    timeout = timeout || 0;
+    setTimeout(() => {
+      this.typed.reset();
+    }, timeout);
   }
 
   handleCollapse(e) {
@@ -21,25 +36,21 @@ class Nav extends Component {
       if ((e.wheelDelta && e.wheelDelta > 0) || (e.detail && e.detail < 0)) {
         if (this.state.collapse !== "") {
           this.setState({ collapse: "" });
-          setTimeout(() => this.typed.reset(), 750);
+          this.retype(750);
         }
       } else {
         if (this.state.collapse !== "collapse") {
           this.setState({ collapse: "collapse" });
-          setTimeout(() => this.typed.reset(), 750);
+          this.retype(750);
         }
       }
     }
   }
 
-  onSetSidebarOpen(open) {
-    this.setState({ sidebarOpen: open });
-  }
-
   componentDidMount() {
     // You can pass other options here, such as typing speed, back speed, etc.
     const options = {
-      strings: ["2645 Laboratory"],
+      strings: [site.banner, site.title],
       typeSpeed: 50,
       backSpeed: 50
     };
@@ -53,55 +64,89 @@ class Nav extends Component {
     this.typed.destroy();
   }
 
+  renderLinks(on) {
+    return nav.links.map((link) => {
+      if ((on === 'sidebar' && link.hideInSidebar) ||
+        (on === 'header' && link.hideInBanner && link.hideInHeader)
+      ) return '';
+      const className = on === 'header' ?
+        link.hideInBanner ? ["collapse-show"] : link.hideInHeader ? ["collapse-hide"] : []
+        : [];
+      const internal = link.path && (link.path.charAt(0) === '/' || link.path.charAt(0) === '#');
+      return internal ? (
+        <li className={className} key={link.name}>
+          <NavLink activeClassName="active" onClick={() => {
+            if (on === 'sidebar') this.setSidebarOpen(false);
+            if (link.typed) this.typed.strings = [link.typed, site.title];
+          }} to={link.path}>{link.name}</NavLink>
+        </li>
+      ) : (
+        <li key={link.name}>
+          <a onClick={on === 'sidebar' ? () => this.setSidebarOpen(false) : null} href={link.path} target="_blank"
+             rel="noopener noreferrer">{link.name}</a>
+        </li>
+      );
+    });
+  }
+
+  renderIcons(on) {
+    return nav.icons.map((icon) => {
+      const internal = icon.path && (icon.path.charAt(0) === '/' || icon.path.charAt(0) === '#');
+      return internal ? (
+        <div className="icon" key={icon.icon}>
+          <NavLink activeClassName="active" onClick={() => {
+            if (on === 'sidebar') this.setSidebarOpen(false);
+            if (icon.typed) this.typed.strings = [icon.typed, site.title];
+          }} to={icon.path} title={icon.title} className={icon.icon}></NavLink>
+        </div>
+      ) : (
+        <div className="icon" key={icon.icon}>
+          <a onClick={on === 'sidebar' ? () => this.setSidebarOpen(false) : null} href={icon.path} title={icon.title}
+             className={icon.icon} target="_blank" rel="noopener noreferrer"></a>
+        </div>
+      );
+    });
+  }
+
   render() {
-    let sidebarContent = <div className="sidebar">
+    const progressbar = (
+      <div id="inb"
+           style={{
+             width: '100%',
+             height: '8px',
+             zIndex: 9999,
+             top: '0px',
+             float: 'left',
+             position: 'absolute'
+           }}>
+        <div
+          style={{
+            backgroundColor: 'rgb(190, 219, 236)',
+            width: '0px',
+            height: '100%',
+            clear: 'both',
+            transition: 'height 0.28s',
+            float: 'left'
+          }}></div>
+      </div>
+    );
+    const sidebarLinks = this.renderLinks.bind(this)('sidebar');
+    const sidebarIcons = this.renderIcons.bind(this)('sidebar');
+    const headerLinks = this.renderLinks.bind(this)('header');
+    const headerIcons = this.renderIcons.bind(this)('header');
+    const sidebarContent = <div className="sidebar">
       <div className="sidebar-banner">
         <h2>
-          (´・ω・`)
+          {nav.sidebar.title}
         </h2>
       </div>
       <div className="top-menu">
         <ul>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/category/tech">技术</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/category/tech/webdev">Web 開發</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/category/tech/operation">運維</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/category/tech/linux">Linux</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/category/tech/desktopdev">桌面開發</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/category/tech/algorithm">算法</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/archives">歸檔</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" onClick={() => this.onSetSidebarOpen(false)} to="/about">關於</NavLink>
-          </li>
+          {sidebarLinks}
         </ul>
       </div>
       <div className="social center">
-        <div className="icon">
-          <a href="" onClick={(e) => e.preventDefault()} title="搜索" className="fas fa-search"></a>
-        </div>
-        <div className="icon">
-          <Link to="/rss" title="RSS" onClick={() => this.onSetSidebarOpen(false)} className="fas fa-rss"></Link>
-        </div>
-        <div className="icon">
-          <a href="https://www.cool2645.com" target="_blank" rel="noopener noreferrer" title="2645 工作室"
-             className="fas fa-home"></a>
-        </div>
-        <div className="icon">
-          <a href="" onClick={(e) => e.preventDefault()} title="主题" className="fas fa-paint-brush"></a>
-        </div>
+        {sidebarIcons}
       </div>
     </div>;
     let sidebarStyle = {
@@ -117,30 +162,21 @@ class Nav extends Component {
       <Router>
         <Sidebar styles={sidebarStyle} sidebar={sidebarContent}
                  open={this.state.sidebarOpen}
-                 onSetOpen={this.onSetSidebarOpen}>
-          <div id="inb"
-               style={{ width: '100%', height: '8px', zIndex: 9999, top: '0px', float: 'left', position: 'absolute' }}>
-            <div
-              style={{
-                backgroundColor: 'rgb(190, 219, 236)',
-                width: '0px',
-                height: '100%',
-                clear: 'both',
-                transition: 'height 0.28s',
-                float: 'left'
-              }}></div>
-          </div>
+                 onSetOpen={this.setSidebarOpen}>
+          {progressbar}
           <div className="top">
             <div className="top-banner"></div>
             <div className="header container center">
               <div className="mobile-show icon">
                 <a href="" onClick={(e) => {
-                  this.onSetSidebarOpen(true);
+                  this.setSidebarOpen(true);
                   e.preventDefault()
-                }} title="搜索" className="fas fa-bars"></a>
+                }} title="菜单" className="fas fa-bars"></a>
               </div>
               <div className={`logo-container ${this.state.collapse}`}>
-                <NavLink exact to="/" activeClassName="active">
+                <NavLink exact to="/" onClick={() => {
+                  this.typed.strings = [site.banner, site.title];
+                }} activeClassName="active">
                   <h1><span
                     style={{ whiteSpace: 'pre' }}
                     ref={(el) => {
@@ -152,46 +188,11 @@ class Nav extends Component {
               <div className={`nav-container ${this.state.collapse}`}>
                 <div className="top-menu">
                   <ul>
-                    <li className="collapse-show">
-                      <NavLink activeClassName="active" to="/category/tech">技术</NavLink>
-                    </li>
-                    <li className="collapse-hide">
-                      <NavLink activeClassName="active" to="/category/tech/webdev">Web 開發</NavLink>
-                    </li>
-                    <li className="collapse-hide">
-                      <NavLink activeClassName="active" to="/category/tech/operation">運維</NavLink>
-                    </li>
-                    <li className="collapse-hide">
-                      <NavLink activeClassName="active" to="/category/tech/linux">Linux</NavLink>
-                    </li>
-                    <li className="collapse-hide">
-                      <NavLink activeClassName="active" to="/category/tech/desktopdev">桌面開發</NavLink>
-                    </li>
-                    <li className="collapse-hide">
-                      <NavLink activeClassName="active" to="/category/tech/algorithm">算法</NavLink>
-                    </li>
-                    <li>
-                      <NavLink activeClassName="active" to="/archives">歸檔</NavLink>
-                    </li>
-                    <li>
-                      <NavLink activeClassName="active" to="/about">關於</NavLink>
-                    </li>
+                    {headerLinks}
                   </ul>
                 </div>
                 <div className="social">
-                  <div className="icon">
-                    <a href="" onClick={(e) => e.preventDefault()} title="搜索" className="fas fa-search"></a>
-                  </div>
-                  <div className="icon">
-                    <Link to="/rss" title="RSS" className="fas fa-rss"></Link>
-                  </div>
-                  <div className="icon">
-                    <a href="https://www.cool2645.com" target="_blank" rel="noopener noreferrer" title="2645 工作室"
-                       className="fas fa-home"></a>
-                  </div>
-                  <div className="icon">
-                    <a href="" onClick={(e) => e.preventDefault()} title="主题" className="fas fa-paint-brush"></a>
-                  </div>
+                  {headerIcons}
                 </div>
               </div>
             </div>
---
+++ src/config.js
@@ -0,0 +1,85 @@
+const site = {
+  name: "2645 实验室",
+  banner: "2645 实验室",
+  title: "2645 Laboratory"
+};
+const nav = {
+  sidebar: {
+    title: "(´・ω・`)"
+  },
+  links: [
+    {
+      name: "技術",
+      typed: "技术",
+      hideInBanner: true,
+      path: "/category/tech"
+    },
+    {
+      name: "Web 開發",
+      typed: "Web 开发",
+      hideInHeader: true,
+      path: "/category/tech/webdev"
+    },
+    {
+      name: "運維",
+      typed: "运维",
+      hideInHeader: true,
+      path: "/category/tech/operation"
+    },
+    {
+      name: "Linux",
+      typed: "Linux",
+      hideInHeader: true,
+      path: "/category/tech/linux"
+    },
+    {
+      name: "桌面開發",
+      typed: "桌面开发",
+      hideInHeader: true,
+      path: "/category/tech/operation"
+    },
+    {
+      name: "算法",
+      typed: "算法",
+      hideInHeader: true,
+      path: "/category/tech/algorithm"
+    },
+    {
+      name: "歸檔",
+      typed: "归档",
+      path: "/archives"
+    },
+    {
+      name: "關於",
+      typed: "关于",
+      path: "/about"
+    },
+  ],
+  icons: [
+    {
+      title: "搜索",
+      typed: "搜索",
+      icon: "fas fa-search",
+      path: "/search",
+    },
+    {
+      title: "RSS",
+      typed: "RSS",
+      icon: "fas fa-rss",
+      path: "/rss",
+    },
+    {
+      title: "2645 工作室",
+      icon: "fas fa-home",
+      path: "https://www.cool2645.com",
+    },
+    {
+      title: "主题",
+      typed: "主题",
+      icon: "fas fa-paint-brush",
+      path: "/theme",
+    }
+  ]
+};
+
+export { site, nav }
