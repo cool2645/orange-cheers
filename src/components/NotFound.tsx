3ea48a2f0957c1a30bd7ddddea6a06f8bba4d370
LI JIAHAO <lijiahao99131@gmail.com>
2018-06-17 22:46:18 +0800

    :lipstick: improve 404 view a little
    Signed-off-by: LI JIAHAO <lijiahao99131@gmail.com>

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
+import '../styles/Loader.css';
+
 class NotFound extends Component {
   public render() {
-    return <h1>404 Not Found</h1>;
+    return  (
+      <div className="container page">
+        <div className="page-container">
+          <div className="full-page-loader">
+            <h1>404 Not Found</h1>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
