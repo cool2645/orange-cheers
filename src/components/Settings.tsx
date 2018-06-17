f01ae19c20f604145ee1f2dba8391ee6790170cd
LI JIAHAO <lijiahao99131@gmail.com>
2018-06-18 01:54:35 +0800

    :globe_with_meridians: i18nlize settings
    Signed-off-by: LI JIAHAO <lijiahao99131@gmail.com>

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-
-import themes from '../themes';
+import { translate, InjectedTranslateProps } from 'react-i18next';
 
 import '../styles/Settings.css';
+import themes from '../themes';
+
+import { INavControlProps } from './Nav';
 
 enum RefreshLevel {
   Always,
@@ -31,36 +33,13 @@ const DefaultRefreshConfig = {
 };
 
 const RefreshSettings = {
-  indexes: [
-    { level: RefreshLevel.Always, prompt: '拉取最新的文章列表数据。' },
-    { level: RefreshLevel.Cache, prompt: '警告：将不会更新文章列表。' },
-  ],
-  posts: [
-    { level: RefreshLevel.Always, prompt: '显示文章页面时重新拉取最新的文章数据。' },
-    { level: RefreshLevel.Cache, prompt: '显示文章页面时不会拉取文章更新，文章将会在刷新文章列表时更新。' },
-  ],
-  categories: [
-    { level: RefreshLevel.Always, prompt: '总是重新拉取分类数据。' },
-    { level: RefreshLevel.Cache, prompt: '不更新已知分类数据。' },
-  ],
-  tags: [
-    { level: RefreshLevel.Always, prompt: '总是重新拉取标签数据。' },
-    { level: RefreshLevel.Cache, prompt: '不更新已知标签数据。' },
-  ],
-  commentCounts: [
-    { level: RefreshLevel.Always, prompt: '总是重新拉取评论数。' },
-    { level: RefreshLevel.Cache, prompt: '不更新已加载过的评论数。' },
-    { level: RefreshLevel.Never, prompt: '不显示评论数。' },
-  ],
-  siblings: [
-    { level: RefreshLevel.Always, prompt: '总是重新拉取相邻的文章。' },
-    { level: RefreshLevel.Cache, prompt: '如果能够从缓存的文章列表中读取相邻的文章，则使用此数据。' },
-    { level: RefreshLevel.Never, prompt: '不显示相邻的文章。' },
-  ],
-  comments: [
-    { level: RefreshLevel.Always, prompt: '总是重新拉取文章评论。' },
-    { level: RefreshLevel.Never, prompt: '不显示文章评论。' },
-  ],
+  indexes: [ RefreshLevel.Always, RefreshLevel.Cache  ],
+  posts: [ RefreshLevel.Always, RefreshLevel.Cache ],
+  categories: [ RefreshLevel.Always, RefreshLevel.Cache ],
+  tags: [ RefreshLevel.Always, RefreshLevel.Cache ],
+  commentCounts: [ RefreshLevel.Always, RefreshLevel.Cache, RefreshLevel.Never ],
+  siblings: [ RefreshLevel.Always, RefreshLevel.Cache, RefreshLevel.Never ],
+  comments: [ RefreshLevel.Always, RefreshLevel.Never ],
 };
 
 function initSettings() {
@@ -69,12 +48,7 @@ function initSettings() {
   if (!localStorage.refreshConfig) localStorage.refreshConfig = JSON.stringify(DefaultRefreshConfig);
 }
 
-interface ISettingsProps {
-  startProgress(): void;
-
-  joinProgress(): void;
-
-  doneProgress(): void;
+interface ISettingsProps extends InjectedTranslateProps, INavControlProps {
 }
 
 interface ISettingsState {
@@ -139,6 +113,7 @@ class Settings extends Component<ISettingsProps, ISettingsState> {
   }
 
   public render() {
+    const { t } = this.props;
     const clearCache = (e: React.MouseEvent<HTMLInputElement>) => {
       e.preventDefault();
       this.clearCache();
@@ -147,7 +122,7 @@ class Settings extends Component<ISettingsProps, ISettingsState> {
       <div className="container page">
         <div className="page-container settings page-box">
           <div className="page-control">
-            <h1>主题</h1>
+            <h1>{t('theme.title')}</h1>
             <ul className="themes nf">
               {
                 themes.themes.map(theme => {
@@ -159,7 +134,7 @@ class Settings extends Component<ISettingsProps, ISettingsState> {
                   return (<li key={theme} className={`${theme} ${active}`}>
                     <a href="" className="nf" onClick={setTheme}>
                       <div className="color" />
-                      <div className="name">{theme}</div>
+                      <div className="name">{t(`theme.${theme}`)}</div>
                     </a>
                   </li>);
                 })
@@ -167,36 +142,34 @@ class Settings extends Component<ISettingsProps, ISettingsState> {
             </ul>
           </div>
           <div className="page-control">
-            <h1>流量节省</h1>
+            <h1>{t('datasaving.title')}</h1>
             {
               Object.keys(RefreshSettings).map(key =>
                 <div key={key}>
-                  <h2>{key}</h2>
+                  <h2>{t(`${key}.name`)}</h2>
                   <p>
                     {
-                      RefreshSettings[key].filter((level: { level: RefreshLevel, prompt: string }) =>
-                        level.level === this.state.refreshConfig[key]).map((level: { level: RefreshLevel, prompt: string }) => level.prompt)
+                      RefreshSettings[key].filter((level: RefreshLevel) =>
+                        level === this.state.refreshConfig[key]).map((level: RefreshLevel) => t(`${key}.${level}.prompt`))
                     }
                   </p>
                   <ul className="nf">
                     {
-                      RefreshSettings[key].map((level: { level: RefreshLevel, prompt: string }) => {
-                        const active = level.level === this.state.refreshConfig[key] ? 'active' : '';
+                      RefreshSettings[key].map((level: RefreshLevel) => {
+                        const active = level === this.state.refreshConfig[key] ? 'active' : '';
                         const setRefreshConfig = (e: React.MouseEvent<HTMLAnchorElement>) => {
                           e.preventDefault();
-                          this.setRefreshConfig(key, level.level);
+                          this.setRefreshConfig(key, level);
                         };
-                        return (<li key={level.level} className={active}>
+                        return (<li key={level} className={active}>
                             <a href="" className="nf" onClick={setRefreshConfig}>
                               <div className="color" />
                               <div className="name">
                                 {
-                                  level.level === RefreshLevel.Always ? '总是刷新' :
-                                    level.level === RefreshLevel.Cache ? '使用缓存' :
-                                      '禁用'
+                                  t(`${key}.${level}.level`)
                                 }
                                 {
-                                  level.level === DefaultRefreshConfig[key] ? '（默认）' : ''
+                                  level === DefaultRefreshConfig[key] ? t('default') : ''
                                 }
                               </div>
                             </a>
@@ -210,8 +183,8 @@ class Settings extends Component<ISettingsProps, ISettingsState> {
             }
           </div>
           <div className="page-control">
-            <h1>清除所有缓存</h1>
-            <input type="button" disabled={this.state.unclearable} onClick={clearCache} value="清空本地缓存" />
+            <h1>{t('clearcache.title')}</h1>
+            <input type="button" disabled={this.state.unclearable} onClick={clearCache} value={t('clearcache.btn')} />
           </div>
         </div>
       </div>
@@ -220,4 +193,4 @@ class Settings extends Component<ISettingsProps, ISettingsState> {
 }
 
 export { RefreshLevel, IRefreshConfig, initSettings };
-export default Settings;
+export default translate('settings')(Settings);
